3fca35eb9e6751960047e7e1996f6eda704b78de
nicolas thouchkaieff <nicolas.thouchkaieff@epitech.eu>
2017-12-27 16:34:52 +0100

    pseudo

--- routes/clock.js
+++ routes/clock.js
@@ -5,9 +5,9 @@
  */
 exports.message = function (req, res, next) {
 
-	if (req.params.user) {
-		req.params.user = req.params.user.trim().substr(0,8);
-	} else req.params.user = '';
+	if (req.params.user && req.params.user.trim() != '')  {
+		req.params.user = req.params.user.trim().substr(0, 8);
+	} else return res.json(400, 'Error: Alors non, tu mets ton pseudo, tout de suite.');
 
 	let icon = 3143;
 
@@ -15,15 +15,15 @@ exports.message = function (req, res, next) {
 		case 'equinox':
 			icon = 16341;
 			break;
-	
+
 		default:
 			break;
 	}
 
 	if (!req.params.message || req.params.message.trim() == '') {
-		res.json(400, 'Error: Tu peux entrer un message stp?');
+		return res.json(400, 'Error: Tu peux entrer un message stp?');
 	} else if (req.params.message.length > 20) {
-		res.json(400, "Error: Tu peux pas entrer de message de plus de 20 characteres psk sinon c'est chiant a lire les pixels 'fin voila voila");
+		return res.json(400, "Error: Tu peux pas entrer de message de plus de 20 characteres psk sinon c'est chiant a lire les pixels 'fin voila voila");
 	} else {
 		_modules.axios.post(`https://maker.ifttt.com/trigger/clock_message/with/key/${config.secret.ifttt.webhookKey}`, {
 			value1: req.params.message.trim(),
